6ef5b65f47eaf0158946323370cafc9fc5e007f4
AntonLayt <anton2636@mail.ru>
2017-01-26 20:54:53 +0300

    Commit [1]

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,8 +1,21 @@
 
 <div class="navbar navbar-default navbar-fixed-bottom">
     <div class="container">
-        <p class="text-center">© Ещё не все права защищены</p>
-        <p class="text-center">Но мы будем стараться</p>
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 
+     	<div class="row">
+
+            <div class="col-md-6 .col-md-offset-3">
+                <ul class="social">
+                	<li class="facebook"><a href="https://facebook.com"><i class="fa fa-facebook fa-3x"></i></a></li>
+                	<li class="twitter"><a href="https://twitter.com/"><i class="fa fa-twitter fa-3x"></i></a></li>
+                	<li class="vk"><a href="https://vk.com"><i class="fa fa-vk fa-3x"></i></a></li>
+                </ul>
+          	</div>
+		</div>
+		<div class="row">
+			<p class="text-right">© Ещё не все права защищены</p>
+	        <p class="text-right">Но мы будем стараться</p>
+        </div>
     </div>
 </div>
